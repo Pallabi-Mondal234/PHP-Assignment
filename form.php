2c5a5f2551a6543785dbcfad5d7df5937429b9a6
Pallabi-Mondal234 <pallabi.mondal@innoraft.com>
2025-03-31 17:48:29 +0530

    FRES-74: Add second task of php assignment.

--- form.php
+++ form.php
@@ -4,6 +4,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fullName=htmlspecialchars($fullName);
 }
 
+//show image
+$target_dir = "upload/";
+$target_file = $target_dir . basename($_FILES["chooseImg"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["chooseImg"]["tmp_name"]);
+    if($check !== false) {
+      echo "File is an image - " . $check["mime"] . ".";
+      $uploadOk = 1;
+    } else {
+      echo "File is not an image.";
+      $uploadOk = 0;
+    }
+  }
+  
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+  
+  // Check file size
+  if ($_FILES["chooseImg"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+  
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+  } 
+
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+} else {
+  if (move_uploaded_file($_FILES["chooseImg"]["tmp_name"], $target_file)) {
+    echo "<img class=uploadedImg src='$target_file' alt='uploaded-img'>";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,3 +65,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </h2>
 </body>
 </html>
+
