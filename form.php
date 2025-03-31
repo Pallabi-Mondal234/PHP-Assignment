4cdcb1c31dfb191bdb9e5e96e1cee0c51187e720
Pallabi-Mondal234 <pallabi.mondal@innoraft.com>
2025-03-31 17:11:04 +0530

    FRES-74 :some changes are added.

--- form.php
+++ form.php
@@ -4,21 +4,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fullName=htmlspecialchars($fullName);
 }
 
-//show image
-$target_dir = "upload/";
-$target_file = $target_dir . basename($_FILES["UploadImg"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-} else {
-  if (move_uploaded_file($_FILES["UploadImg"]["tmp_name"], $target_file)) {
-    echo "<img src='$target_file' alt='uploaded-img'>";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
