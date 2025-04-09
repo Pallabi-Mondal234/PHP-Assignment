ab686637d864cf7a757935600742fae0826ae5c1
Pallabi-Mondal234 <pallabi.mondal@innoraft.com>
2025-04-09 10:39:03 +0530

    FRES-74: Add logout button after form submission.

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <?php
+include '../Login/logout.php';
 
 class FormHandler
 {
@@ -76,6 +77,7 @@ $formHandler->handleFormSubmission();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Output</title>
     <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="/PHP-Assignment-New/Login/CSS/style.css">
 </head>
 
 <body>
